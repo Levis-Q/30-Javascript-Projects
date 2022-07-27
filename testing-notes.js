d91692a7a3ea8112765cf72caa7b342975c223f4
LeviisQ <97918225+Levis-Q@users.noreply.github.com>
2022-07-27 02:28:56 +0200

    Add files via upload

---
+++ testing-notes.js
@@ -0,0 +1,35 @@
+/*  
+// function declaration
+
+function checkIfNameExists(name) {
+
+// Array of Nmaes Declaration
+    let names = ['Mgijima', 'Kabelo', 'Shangwan', 'Reiko', 'John', 'Mnyokza']    
+
+// check for matching names
+    if(names.includes(name)) {
+        return true;
+    }else {
+        return false;
+    }
+}
+ 
+checkIfNameExists("Reiko");  // ANS1 DOESN'T RETURN FEEDBACK
+
+
+
+function checkIfNameExists() {
+    let names = ['Mgijima', 'Kabelo', 'Shangwan', 'Reiko', 'John', 'Mnyokza']
+
+// loop and check for matching names
+    for (let i = 0; i < names.length; i++) {
+    if(names[i] === name) {
+        return true;
+    }
+    return false;
+    }
+}
+
+checkIfNameExists('Mnyokza');  // ANS2 DOESN'T RETURN FEEDBACK either
+  */
+
